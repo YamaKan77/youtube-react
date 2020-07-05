71ef6767145ec62f2b679ae79023387118f68b36
Kan Yamamoto <kanyamamoto77@gmail.com>
2020-07-05 15:18:03 -0700

    Added watch screen template

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import HeaderNav from './containers/HeaderNav/HeaderNav';
-import SideBar from './containers/SideBar/SideBar';
-import Home from './containers/Home/Home';
-import './containers/Home/Home.scss';
+import {Home} from './containers/Home/Home';
+import Watch from './containers/Watch/Watch';
+import AppLayout from './components/AppLayout/AppLayout';
+import {Route, Switch} from 'react-router-dom';
 
 class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <HeaderNav/>
-        <SideBar/>
-        <Home/>
-      </React.Fragment>
-      );
-  }
+	render() {
+		return (
+			<AppLayout>
+				<Switch>
+					<Route path="/watch" component={Watch}/>
+					<Route path="/" component={Home}/>
+				</Switch>
+			</AppLayout>
+			);
+	}
 }
 
 export default App;
---
+++ src/containers/Watch/Watch.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Watch.scss';
+import {Video} from '../../components/Video/Video';
+import {VideoPreview} from '../../components/VideoPreview/VideoPreview';
+
+export class Watch extends React.Component {
+  render() {
+    return (
+    	<React.Fragment>
+    		<VideoPreview horizontal={true}/>
+    		<VideoPreview/>
+    	</React.Fragment>
+    );
+  }
+}
+
+export default Watch;
