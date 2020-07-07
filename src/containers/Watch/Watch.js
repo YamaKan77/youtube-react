6bf2f96a9f88f169e4d387413e77982340380baa
Kan Yamamoto <kanyamamoto77@gmail.com>
2020-07-07 14:49:50 -0700

    Added comments section

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -4,6 +4,7 @@ import {Video} from '../../components/Video/Video';
 import {RelatedVideos} from '../../components/RelatedVideos/RelatedVideos';
 import {VideoMetadata} from '../../components/VideoMetadata/VideoMetadata';
 import {VideoInfoBox} from '../../components/VideoInfoBox/VideoInfoBox';
+import {Comments} from '../Comments/Comments';
 
 export class Watch extends React.Component {
   render() {
@@ -12,7 +13,7 @@ export class Watch extends React.Component {
     		<Video className='video' id='-7fuHEEmEjs' />
     		<VideoMetadata className='metadata' viewCount={1000}/>
     		<VideoInfoBox className='video-info-box' />
-        <div className='comments' style={{width: '100%', height: '100px', background: '#9013FE'}}>comments</div>
+        <Comments className='comments' amountComments={5}/>
         <RelatedVideos className='relatedVideos'/>
     	</div>
     );
