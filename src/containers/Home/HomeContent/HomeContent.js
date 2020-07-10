6bc947d8b3d974023a5f5ba610d638bef3a6a291
Kan Yamamoto <kanyamamoto77@gmail.com>
2020-07-10 16:27:40 -0700

    Home page populates popular videos

--- src/containers/Home/HomeContent/HomeContent.js
+++ src/containers/Home/HomeContent/HomeContent.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import './HomeContent.scss';
 import {VideoGrid} from '../../../components/VideoGrid/VideoGrid';
+import {getMostPopularVideos} from '../../../store/reducers/videos';
+import {connect} from 'react-redux';
+
+const AMOUNT_TRENDING_VIDEOS = 12;
 
 export class HomeContent extends React.Component {
 	render() {
+		const trendingVideos = this.getTrendingVideos();
 		return (
 			<div className='home-content'>
 				<div className="responsive-video-grid-container">
-					<VideoGrid title='Trending'/>
-					<VideoGrid title='Autos & Vehicles' hideDivider={true}/>
+					<VideoGrid title='Trending' videos={trendingVideos}/>
 				</div>
 			</div>
 		);
 	}
+
+	getTrendingVideos() {
+		return this.props.getMostPopularVideos.slice(0, AMOUNT_TRENDING_VIDEOS);
+	}
 }
 
-export default HomeContent;
+function mapStateToProps(state) {
+	return {
+		getMostPopularVideos: getMostPopularVideos(state),
+	};
+}
+
+export default connect(mapStateToProps, null)(HomeContent);
